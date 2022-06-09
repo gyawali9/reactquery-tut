6fa1e837f572758ba91e69ebd1604fc3033db863
gyawali9 <roshan.gyawali@codehimalaya.com>
2022-06-09 17:39:18 +0545

    useEffect and axios: traditional data fetch

--- src/components/SuperHeroesPage.js
+++ src/components/SuperHeroesPage.js
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const SuperHeroesPage = () => {
-  return <div>SuperHeroes.page</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      // console.log(res.data);
+      setData(res.data);
+      setIsLoading(false);
+      console.log(data);
+    });
+  }, []);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {/* {data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))} */}
+      {/* {data.map((hero) => {
+        return (
+          <>
+            <div key={hero.name}>{hero.name}</div>
+            <p>1</p>
+          </>
+        );
+      })} */}
+      {data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </>
+  );
 };
 
 export default SuperHeroesPage;
