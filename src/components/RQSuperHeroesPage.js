fa6461c197df183842d35a2ccc50aa45958e7ad7
gyawali9 <roshan.gyawali@codehimalaya.com>
2022-06-12 17:49:48 +0545

    url correction for both traditional and useQuery fetch

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroe");
+  return axios.get("http://localhost:4000/superheroes");
 };
 
 const RQSuperHeroesPage = () => {
--- src/components/SuperHeroesPage.js
+++ src/components/SuperHeroesPage.js
@@ -7,7 +7,7 @@ const SuperHeroesPage = () => {
   const [error, setError] = useState("");
   useEffect(() => {
     axios
-      .get("http://localhost:4000/superheroe")
+      .get("http://localhost:4000/superheroes")
       .then((res) => {
         setData(res.data);
         setIsLoading(false);
