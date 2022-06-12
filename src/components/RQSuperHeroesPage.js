e2e847c33bfa9aadca0607b9cf198e7d7cd462ce
gyawali9 <roshan.gyawali@codehimalaya.com>
2022-06-12 17:47:49 +0545

    useQuery error handling

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -2,14 +2,20 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroe");
 };
 
 const RQSuperHeroesPage = () => {
   // 2nd argument is function that returns promise
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes
+  );
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
   return (
     <>
